1c5f9ea50028dda4869e4693b6fc48730194abd0
Shion Ichikawa <shion1305@gmail.com>
2024-03-24 03:25:22 +0900

    ✨ inquiry-form.go, for new schema

--- svc/pkg/schema/inquiry-form.go
+++ svc/pkg/schema/inquiry-form.go
@@ -1,8 +1,39 @@
 package schema
 
+import "errors"
+
 type InquiryData struct {
-	Name           string `json:"name"`
-	CompanyName    string `json:"company"`
-	EmailAddress   string `json:"email"`
-	InquiryDetails string `json:"inquiry"`
+	Firstname       string                 `json:"firstname"`
+	Lastname        string                 `json:"lastname"`
+	CompanyName     string                 `json:"company"`
+	EmailAddress    string                 `json:"email"`
+	BusinessContent string                 `json:"business"`
+	InquiryDetails  string                 `json:"inquiry"`
+	UseSlack        bool                   `json:"use_slack"`
+	SlackInfo       *[]OtherSlackPersonnel `json:"slack_other"`
+}
+
+type OtherSlackPersonnel struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+}
+
+func (i InquiryData) Validate() error {
+	if i.Firstname == "" {
+		return errors.New("firstname is required")
+	}
+	if i.Lastname == "" {
+		return errors.New("lastname is required")
+	}
+	if i.CompanyName == "" {
+		return errors.New("company name is required")
+	}
+	if i.EmailAddress == "" {
+		return errors.New("email address is required")
+	}
+	if i.UseSlack && (i.SlackInfo == nil || len(*i.SlackInfo) == 0) {
+		return errors.New("slack info is required")
+	}
+	return nil
 }

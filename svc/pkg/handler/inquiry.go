a2884b5aed565b6a4e04f0a53e09a54f485b6a10
Shion Ichikawa <shion1305@gmail.com>
2024-03-24 03:35:21 +0900

    ✨ handler/inquiry impl Slack Invitation

--- svc/pkg/handler/inquiry.go
+++ svc/pkg/handler/inquiry.go
@@ -2,14 +2,23 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
+	"kitakyusyu-hackathon/pkg/slack"
 	"kitakyusyu-hackathon/svc/pkg/schema"
+	"kitakyusyu-hackathon/svc/pkg/uc"
+	"log"
 )
 
 type InquiryHandler struct {
+	slackClient *slack.Slack
+	inviteUC    *uc.InviteSlack
 }
 
 func NewInquiryHandler() *InquiryHandler {
-	return &InquiryHandler{}
+	s := slack.NewSlack()
+	return &InquiryHandler{
+		slackClient: &s,
+		inviteUC:    uc.NewInviteSlack(s),
+	}
 }
 
 func (h *InquiryHandler) HandleInquiry() gin.HandlerFunc {
@@ -21,6 +30,37 @@ func (h *InquiryHandler) HandleInquiry() gin.HandlerFunc {
 				"message": err.Error(),
 			})
 			return
+		}
+
+		if err := data.Validate(); err != nil {
+			c.JSON(400, gin.H{
+				"status":  false,
+				"message": err.Error(),
+			})
+			return
+		}
+
+		if data.UseSlack {
+			guests := make([]uc.GuestInfo, 0, len(*data.SlackInfo))
+			for _, s := range *data.SlackInfo {
+				guests = append(guests, uc.GuestInfo{
+					Email:     s.Email,
+					Firstname: s.Firstname,
+					Lastname:  s.Lastname,
+				})
+			}
+
+			inviteInput := uc.InviteSlackInput{
+				ChannelName: data.CompanyName,
+				StaffIDs:    []string{"U04936U1UEB"},
+				GuestInfo:   guests,
+			}
+			inviteResult, err := h.inviteUC.Do(inviteInput)
+			if err != nil {
+				return
+			}
+			log.Printf("channel created: %s\n", inviteResult.ChannelName)
+			log.Printf("channel link: %s\n", inviteResult.ChannelLink)
 		}
 
 		c.JSON(200, gin.H{
